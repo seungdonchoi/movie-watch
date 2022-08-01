1298f9a11571d117a8d93b794bd09cea56eadb6d
seungdonchoi <dchoifor2@gmail.com>
2022-08-01 03:34:09 -0700

    added dummy data

--- server/db/index.js
+++ server/db/index.js
@@ -4,17 +4,6 @@ const dbConnection = new Sequelize(
   "postgres://localhost:5432/moviewatchlist"
 );
 
-/*
-  Movie model
-    - title(not null);
-    - imdbLink(null);
-    - watched(not null, boolean, default false);
-  Genre model
-    - name(not null);
-  Many-to-many movies and genres
-  belongsToMany
-*/
-
 const Movie = dbConnection.define("movie", {
   title: {
     type: Sequelize.DataTypes.STRING(255),
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,7 +1,36 @@
-const dbConnection = require("./index").dbConnection;
+const {dbConnection, Movie, Genre} = require("./index");
 
 const runSeed = async () => {
   await dbConnection.sync({ force: true});
+  const horrorGenre = await Genre.create({name: 'Horror'});
+  const comedyGenre = await Genre.create({name: 'Comedy'});
+  const sciFiGenre = await Genre.create({name: 'Science Fiction'});
+  const dramaGenre = await Genre.create({name: 'Drama'});
+  const animGenre = await Genre.create({name: 'Animation'});
+  const familyGenre = await Genre.create({name: 'Family'})
+
+  const donnieDarko = await Movie.create({
+    title: 'Donnie Darko',
+    imdbLink: 'https://www.imdb.com/title/tt0246578/'
+  })
+  await donnieDarko.setGenres([horrorGenre, sciFiGenre]);
+
+  const cabinInTheWoods = await Movie.create({
+    title: 'Cabin In The Woods',
+  })
+  await cabinInTheWoods.setGenres([horrorGenre, comedyGenre, sciFiGenre]);
+
+  const corpseBride = await Movie.create({
+    title: 'Corpse Bride',
+    imdbLink: 'https://www.imdb.com/title/tt0121164/?ref_=nv_sr_srsg_0'
+  })
+  await corpseBride.setGenres([animGenre, familyGenre, dramaGenre]);
+
+  const nightmareBeforeChristmas = await Movie.create({
+    title: 'Nightmare Before Christmas'
+  })
+  await nightmareBeforeChristmas.setGenres([familyGenre, animGenre])
+
   console.log('Seeding is complete!')
   process.kill(0);
 }
